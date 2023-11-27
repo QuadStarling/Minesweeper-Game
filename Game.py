24c9f1e988d6cf8ed9e807d941368b7a43ad4ced
Jad Azaizeh <jad462.azizeh@gmail.com>
2023-11-27 20:51:53 +0200

    Added a timer and a counter for flags

--- Game.py
+++ Game.py
@@ -10,27 +10,40 @@ class FirstSafeClick(Enum):
 
 class Game:
     def __init__(self, window):
+        self.__id = None
         self.__window = window
         self.__game_frame = tk.Frame(self.__window, bg="black", width=500, height=500, relief=tk.SUNKEN)
         self.__game_frame.grid(row=1, column=0)
 
         self.__stats_frame = tk.Frame(self.__window)
-        self.__stats_frame.grid(row=0, column=0, pady=3)
+        self.__stats_frame.grid(row=0, column=0, sticky="wens")
+        self.__window.columnconfigure(0, weight=1)
 
-        self.__coverFrame = tk.Frame(self.__window,
-                                     bg='')  # Covers the game frame to block the player from clicking the buttons
+        self.__coverFrame = tk.Frame(self.__window, bg='')  # Covers the game frame to block the player from clicking the buttons
 
         self.__buttons_list = None  # A list to store buttons
         self.__game = None
         self.__resetButton = None  # A button that resets the game
 
         self.__squareCounter = 0  # Counts how many squares have been opened up that are not mines
+        self.__counter_label = None
 
+        self.__flagsLeft = 0
+        self.__flagsLeft_label = None
+
+        self.__time_counter = 0  # A Counter for how many seconds passed since the start of the game
         self.__firstSafeClick = FirstSafeClick.ACTIVE  # Makes sure that the first click is not a mine
 
     def Create_Game(self, width, height, mines):
+        self.__counter_label = tk.Label(self.__stats_frame, bg="black", fg="red", text=f"{self.__time_counter:03}", font=("Arial", 18), width=3)
+        self.__counter_label.pack(side=tk.RIGHT, padx=(0, 10))
+
+        self.__flagsLeft = mines
+        self.__flagsLeft_label = tk.Label(self.__stats_frame, bg="black", fg="red", text=f"{min(self.__flagsLeft, 999):03}", font=("Arial", 18), width=3)
+        self.__flagsLeft_label.pack(side=tk.LEFT, padx=(10, 0))
+
         self.__resetButton = tk.Button(self.__stats_frame, text="O   O\n\\___/", bd=4, bg="yellow")
-        self.__resetButton.pack()
+        self.__resetButton.pack(pady=10)
         self.__resetButton.bind("<Button-1>", lambda event: self.__resetButton.config(text="O   O\n\\___/"))
         self.__resetButton.bind("<ButtonRelease-1>", self.resetGame)
 
@@ -48,11 +61,27 @@ class Game:
             self.__buttons_list[i // height].append(button)
             button.grid(row=(i // height), column=i % width, pady=1, padx=1)
 
+    def update_counter(self):
+        self.__time_counter += 1
+        self.__counter_label.config(text=f"{min(self.__time_counter, 999):03}")
+        self.__id = self.__window.after(1000, self.update_counter)
+
     def resetGame(self, event):
         self.__coverFrame.grid_forget()
         self.__game.clear_board()
         self.__firstSafeClick = FirstSafeClick.ACTIVE
         self.__squareCounter = 0
+
+        # stops and resets the timer
+        self.__time_counter = 0
+        self.__counter_label.config(text=f"{self.__time_counter:03}")
+        if hasattr(self, "__id"):
+            self.__window.after_cancel(self.__id)
+
+        # Reset how many flag left
+        self.__flagsLeft = self.__game.mines
+        self.__flagsLeft_label.config(text=f"{self.__flagsLeft:03}")
+
         for i in range(self.__game.height):
             for j in range(self.__game.width):
                 if (self.__game.get_board()[i][j].flagState == FlagStatus.ON):
@@ -72,17 +101,22 @@ class Game:
         self.__resetButton.config(text="O   O\n O")
 
     def show(self, button):
+        self.__resetButton.config(text="O   O\n\\___/")
+        if (button['state'] == tk.DISABLED):
+            return
         row = button.grid_info()['row']
         column = button.grid_info()['column']
-        self.__resetButton.config(text="O   O\n\\___/")
+
 
         if (self.__firstSafeClick == FirstSafeClick.ACTIVE):
             self.__game.generate_minefield(row, column)
             self.__game.generate_neighbor_info()
+            self.update_counter()
             self.__firstSafeClick = FirstSafeClick.INACTIVE
 
         if (self.__game.get_board()[row][column].has_mine == True):
             self.__buttons_list[row][column].config(bg="red", text='x')
+            self.__window.after_cancel(self.__id)
             for i in range(self.__game.height):
                 for j in range(self.__game.width):
                     if (i == row and j == column):
@@ -94,9 +128,6 @@ class Game:
 
                     elif (self.__game.get_board()[i][j].has_mine == False and self.__game.get_board()[i][j].flagState == FlagStatus.ON):
                         self.__buttons_list[i][j].config(bg="green")
-
-
-
 
             self.__game.get_board()[row][column].status = SquareStatus.OPENED
             self.__resetButton.config(text="X   X\n/``````\\")
@@ -112,7 +143,6 @@ class Game:
                         self.__squareCounter += 1
                         self.__buttons_list[i][j].config(bg="white",
                                                          text='',
-                                                         disabledforeground="blue",
                                                          state=tk.DISABLED)
                     else:
                         self.__squareCounter += 1
@@ -122,13 +152,13 @@ class Game:
                                                          state=tk.DISABLED)
                     if (self.__squareCounter == (self.__game.height * self.__game.width) - self.__game.mines):
                         self.__resetButton.config(text="-   O\n\\___/")
+                        self.__window.after_cancel(self.__id)
                         for r in range(self.__game.height):
                             for c in range(self.__game.width):
                                 if (self.__game.get_board()[r][c].has_mine == True and self.__game.get_board()[r][c].flagState == FlagStatus.OFF):
                                     self.place_flag(self.__buttons_list[r][c])
                         self.__coverFrame.grid(row=1, column=0, sticky="nsew")
                         return
-
 
     def place_flag(self, button):
         row = button.grid_info()['row']
@@ -139,7 +169,11 @@ class Game:
                 button.config(bg="lightgray", state=tk.NORMAL)
                 button.bind("<ButtonRelease-1>", lambda event: self.show(button))
                 self.__game.get_board()[row][col].flagState = FlagStatus.OFF
+                self.__flagsLeft += 1
+                self.__flagsLeft_label.config(text=f"{self.__flagsLeft:03}")
             else:
                 button.config(bg="orange", state=tk.DISABLED)
                 button.bind("<ButtonRelease-1>", lambda event: self.__resetButton.config(text="O   O\n\\___/"))
                 self.__game.get_board()[row][col].flagState = FlagStatus.ON
+                self.__flagsLeft -= 1
+                self.__flagsLeft_label.config(text=f"{max(self.__flagsLeft, -99):03}")

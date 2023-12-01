56d2af81f62eea024b4741c3e4b69aab0c7f6219
Jad Azaizeh <jad462.azizeh@gmail.com>
2023-12-01 16:31:14 +0200

    Added a help menu to the MenuBar

--- MenuBar.py
+++ MenuBar.py
@@ -1,5 +1,6 @@
 import sys
 import tkinter as tk
+from tkinter import messagebox
 from Difficulties import *
 
 
@@ -13,7 +14,6 @@ class MenuBar:
         self.__window.config(menu=self.__menuBar)
 
         self.__x = tk.IntVar()
-        self.__difficulties = [Difficulty.BEGINNER, Difficulty.INTERMEDIATE, Difficulty.EXPERT]
 
         self.__gameMenu = tk.Menu(self.__menuBar, tearoff=0, font=("Arial", 10))
         self.__menuBar.add_cascade(label="Game", menu=self.__gameMenu)
@@ -26,6 +26,12 @@ class MenuBar:
         self.__gameMenu.add_command(label="Exit", command=sys.exit)
 
         self.__game.Create_Game(*Difficulty.BEGINNER.value)  # Default Difficulty for when game starts first time
+
+        self.__helpMenu = tk.Menu(self.__menuBar, tearoff=0, font=("Arial", 10))
+        self.__menuBar.add_cascade(label="Help", menu=self.__helpMenu)
+        self.__helpMenu.add_command(label="How to Play", command=lambda: MenuBar.show_help('How to Play'))
+        self.__helpMenu.add_separator()
+        self.__helpMenu.add_command(label="About Minesweeper...", command=lambda: MenuBar.show_help('About Minesweeper'))
 
     def chooseDiff(self):
         if (self.__x.get() == 0):
@@ -40,3 +46,16 @@ class MenuBar:
             self.__game.Destroy()
             self.__game.Create_Game(*Difficulty.EXPERT.value)
             self.__game.resetGame()
+
+    @staticmethod
+    def show_help(help_type):
+        help_text = ""
+        if help_type == "How to Play":
+            with open("How_to_Play.txt", 'r') as file:
+                help_text = file.read()
+
+        elif help_type == "About Minesweeper":
+            with open("AboutMS.txt", 'r') as file:
+                help_text = file.read()
+
+        tk.messagebox.showinfo(help_type, help_text)
